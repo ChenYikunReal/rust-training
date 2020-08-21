e2303707318dcf7d9727b7ebd701cff38c08d9d0
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-08-22 00:09:15 +0800

    修改项目结构

---
+++ projects/hello/target/debug/deps/hello-42218f3be8e798d7.d
@@ -0,0 +1,5 @@
+D:\IDEA\rust_training\projects\hello\target\debug\deps\hello-42218f3be8e798d7.exe: src\main.rs
+
+D:\IDEA\rust_training\projects\hello\target\debug\deps\hello-42218f3be8e798d7.d: src\main.rs
+
+src\main.rs:
---
+++ projects/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+D:\IDEA\rust_training\projects\hello\target\debug\hello.exe: D:\IDEA\rust_training\projects\hello\src\main.rs
